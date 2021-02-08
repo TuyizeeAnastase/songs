59e3d917fc80813e95537a49a2510f18a7b23685
TuyizeeAnastase <tuyizereanastase1@gmail.com>
2021-02-08 19:04:18 +0200

    selectio songs

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 
-export const selectSong=()=>{
+export const selectSong=(song)=>{
     return {
        type:'SONG_SELECTED',
        payload:song
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from "../actions";
 
 class SongList extends Component{
+    renderList(){
+        return this.props.songs.map((songs)=>{
+            return(
+                <div className="item" key={songs.title}>
+                 <div className="right floated content">
+                   <button 
+                   className="ui button primary"
+                   onClick={()=>this.props.selectSong(songs)}
+                   >
+                      Select
+                    </button>
+                 </div>
+                 <div className="content">{songs.title}</div>
+                </div>
+            );
+        });
+    }
     render(){
-        return <div>SongList</div>
+        return <div className="ui divided list">{this.renderList()}</div>
     }
 }
 
-export default connect()(SongList);
+
+const mapStateToProps=(state)=>{
+console.log(state);
+ return {songs:state.songs};
+}
+
+export default connect(mapStateToProps,
+    {selectSong})
+    (SongList);
